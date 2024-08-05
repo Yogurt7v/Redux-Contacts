ce1f8456e98df378084f41f052b23376fd326967
Yogurt7v <egorov-2k@yandex.ru>
2024-08-05 17:48:51 +0300

    fix: добравил отображение пользователя после обновления страницы.

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,21 +1,25 @@
-import { useEffect, useState, memo } from 'react';
+import { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { ContactDto } from 'src/types/dto/ContactDto';
 import { ContactCard } from 'src/components/ContactCard';
 import { Empty } from 'src/components/Empty';
 import { ContactsStore } from 'src/store/contactsStore';
+import { observer } from 'mobx-react-lite';
 
 
-export const ContactPage = memo(() => {
-  const contactsState = useState<ContactDto[] | undefined>(ContactsStore.contacts)
+export const ContactPage = observer(() => {
   const { contactId } = useParams<{ contactId: string }>();
   const [contact, setContact] = useState<ContactDto>();
+  const contactsState = ContactsStore.contacts
 
   useEffect(() => {
-    setContact(() => contactsState[0]?.find(({ id }) => id === contactId));
+    setContact(() => ContactsStore.contacts.find(({ id }) => id === contactId));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [contactId]);
+  }, [contactId, contactsState]);
+
+
+  if (!contact) return <p>Loading ...</p>
 
   return (
     <Row xxl={3}>
